fdab2899444a3a4c3e68bf9b96e65e8c11b4c093
Alvin <matarozzo@automationware.it>
2024-03-15 11:12:16 +0100

    add gitignore

--- launch/sick_flexysoft_launch.py
+++ launch/sick_flexysoft_launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             output='screen',
             emulate_tty=True,
             parameters=[
-                {'hostname': '192.168.250.251'},
+                {'hostname': '192.168.250.250'},
                 {'broadcast': '192.168.255.255'}
             ]
         )
