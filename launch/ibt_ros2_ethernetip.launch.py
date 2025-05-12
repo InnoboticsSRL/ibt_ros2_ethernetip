dfb3f87c68787dc55a792c74c4b4d9fe14585ef0
Mattia Dei Rossi <mattydr98@gmail.com>
2025-05-12 12:32:33 +0200

    launch fixed

--- launch/ibt_ros2_ethernetip.launch.py
+++ launch/ibt_ros2_ethernetip.launch.py
@@ -1,16 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
 
 def generate_launch_description():
+
+    ns = LaunchConfiguration('ns')
+    ip = LaunchConfiguration('ip')
+
+    declare_namespace = DeclareLaunchArgument('ns', default_value='sick_gateway', description='Namespace for the node')
+    declare_ip = DeclareLaunchArgument(
+        'ip', default_value='192.168.250.250', description='IP address of the device')
+
+    node = Node(
+        package='ibt_ros2_ethernetip',
+        executable='ibt_ros2_ethernetip',
+        name='ibt_ros2_ethernetip_node',
+        namespace=ns,
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+                {'hostname': ip},
+        ]
+    )
+
     return LaunchDescription([
-        Node(
-            package='sick_flexysoft_ethernetip',
-            executable='sick_flexysoft',
-            name='sick_flexysoft',
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                {'hostname': '192.168.250.250'},
-            ]
-        )
+        declare_ip,
+        declare_namespace,
+
+        node
     ])
